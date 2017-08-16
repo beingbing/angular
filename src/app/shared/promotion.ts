8321404410e018a06b1b09a430babc0e26a76c7e
Samar <shaikhsamartaj@gmail.com>
2017-08-17 03:49:30 +0530

    single page application part 1

--- src/app/shared/promotion.ts
+++ src/app/shared/promotion.ts
@@ -1,13 +1,9 @@
-import { Comments } from './comment';
-
-export class Dish {
-	id: number;
+export class Promotion {
+    id: number;
     name: string;
     image: string;
-    category: string;
     label: string;
     price: string;
     featured: boolean;
     description: string;
-    comments: Comments[];
 }
